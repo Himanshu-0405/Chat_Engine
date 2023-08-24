537bd381814b2a47c8ed5733e3fedecda820516a
Himanshu <ranjanhimanshu2k3@gmail.com>
2023-08-24 21:55:10 +0530

    final changes

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,17 @@
-const ChatsPage = () => {
-    return <div className="background">chats...</div>;
-  };
-  export default ChatsPage; 
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{height:'100vh'}}>
+      <PrettyChatWindow
+        projectId='
+        41748f2f-54f7-428f-a86b-8123da177083'
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{height:'100%'}}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
